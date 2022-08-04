4be7c5b5453ffb9da3b55ab98873b33ae3b29eb0
DESKTOP-GFA21RA\DANIELA <leysonportillo85@gmail.com>
2022-08-04 16:32:07 -0500

    inciar el diseño del logo

---
+++ Decell/app/src/main/java/com/example/de_cell/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.de_cell;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
